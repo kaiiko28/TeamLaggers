b703c8f634cdb2ac6ab8f6aaebb2469efd26e0a1
iorins <iori28zu@gmail.com>
2020-02-22 20:33:48 +0800

    map

--- bluehack2020/resources/views/dictionary/map.blade.php
+++ bluehack2020/resources/views/dictionary/map.blade.php
@@ -1,1 +1,23 @@
-<img src="{{asset('/img/map.png')}}" alt="">
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>map</title>
+</head>
+<body>
+    <img src="{{asset('/img/map.png')}}" alt="map" usemap="#philippines">
+
+<map id="philippines">
+    <area data-toggle="tooltip" title="bikolano" shape="rect" coords="307,309,358,390" href="/" alt="bikolano">
+    <area data-toggle="tooltip" title="bisaya" shape="rect" coords="304,534,359,546" href="/" alt="bisaya">
+    <area data-toggle="tooltip" title="cebuano" shape="rect" coords="368,486,427,206" href="/" alt="cebuano">
+    <area data-toggle="tooltip" title="hiligaynon" shape="rect" coords="381,636,416,568" href="/" alt="hiligaynon">
+    <area data-toggle="tooltip" title="ilocano" shape="rect" coords="187,146,282,206" href="/" alt="ilocano">
+    <area data-toggle="tooltip" title="tagalog" shape="rect" coords="40,275,219,513" href="/" alt="tagalog">
+    <area data-toggle="tooltip" title="tausug" shape="rect" coords="280,620,311,636" href="/" alt="tausug">
+    <area data-toggle="tooltip" title="waray" shape="rect" coords="373,375,433,449" href="/" alt="waray">
+</map>
+</body>
+</html>
---
+++ bluehack2020/resources/views/index.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>home</title>
+    <link rel="stylesheet" href="{{ asset('/mdb/css/bootstrap.css')}}">
+    <link rel="stylesheet" href="{{ asset('/mdb/css/bootstrap.min.css')}}">
+    <link rel="stylesheet" href="{{ asset('/mdb/css/bootstrap.js')}}">
+    <link rel="stylesheet" href="{{ asset('/mdb/css/bootstrap.min.js')}}">
+    <link rel="stylesheet" href="{{ asset('/css/style.css')}}">
+</head>
+<body>
+    {{-- <nav style="background:blue;" class="navbar  navbar-dark primary-color">
+       <a class="navbar-brand" href="#">Title</a>
+       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span></button> 
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li>
+                    <a class="nav-link" href="/dictionary">Dictionary</a>
+                </li>
+                <li>
+                    <a class="nav-link" href="/transportation">Reserve</a>
+                </li>
+            </ul>
+        </div>
+    </nav> --}}
+    <div>
+        <img src="" alt="">
+    </div>
+</body>
+</html>
