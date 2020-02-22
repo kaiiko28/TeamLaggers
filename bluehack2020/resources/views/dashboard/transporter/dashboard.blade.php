28761bcdd362e20c2a9548046baad21ed8500f3f
iorins <iori28zu@gmail.com>
2020-02-22 23:39:05 +0800

    index

---
+++ bluehack2020/resources/views/dashboard/transporter/dashboard.blade.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Transportation</title>
+    <link rel="stylesheet" href="{{ asset('/mdb/css/bootstrap.css')}}">
+    <link rel="stylesheet" href="{{ asset('/mdb/css/bootstrap.min.css')}}">
+    <script src="{{ asset('/mdb/js/jquery-3.4.1.min.js')}}"></script>
+    <script src="{{asset('/mdb/js/bootstrap.js')}}"></script>
+    <script src="{{asset('/mdb/js/bootstrap.min.js')}}"></script>
+    <link rel="stylesheet" href="{{ asset('/css/style.css')}}">
+    
+</head>
+<body>
+    <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img class="d-block w-100" src="{{asset('/img/kalabaw1.jpg')}}"
+              alt="First slide">
+          </div>
+          <div class="carousel-item">
+            <img class="d-block w-100" src="{{asset('/img/kalesa.jpg')}}"
+              alt="Second slide">
+          </div>
+          <div class="carousel-item">
+            <img class="d-block w-100" src="{{asset('/img/habal1.jpg')}}"
+              alt="Third slide">
+          </div>
+          <div class="carousel-item">
+            <img class="d-block w-100" src="{{asset('/img/etrik2.jpg')}}"
+              alt="Fourthslide">
+          </div>
+          <div class="carousel-item">
+            <img class="d-block w-100" src="{{asset('/img/banka1.jpg')}}"
+              alt="Fifth slide">
+          </div>
+        </div>
+        <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="sr-only">Next</span>
+        </a>
+      </div>
+      <div class="container">
+          <div>
+              h1
+          </div>
+
+      </div>
+    <div class="row" style="background:lightblue;">
+        <div class="col-sm-4">
+            <h1>kalabaw</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nam cum tempora doloribus optio incidunt dicta maxime sed omnis facilis. Aliquam incidunt nulla sed consequuntur ea vero voluptates harum ullam placeat?</p>
+        </div>
+        <div class="col-sm-4">
+            <h1>kalesa</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam sed pariatur accusantium modi nihil optio aspernatur nulla rerum beatae aliquam ducimus similique sapiente earum accusamus, quae possimus quis praesentium eaque.</p>
+        </div>
+        <div class="col-sm-4">
+            <h1>habal</h1>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consectetur, quam deserunt reiciendis distinctio quisquam nostrum, rem deleniti, dolore cum quasi officia? Aperiam quaerat reprehenderit incidunt itaque officiis debitis magnam omnis.</p>
+        </div>
+        <div class="col-sm-6">
+            <h1>Etrik</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis sit sed iste temporibus officia labore delectus facere maiores similique exercitationem. Consectetur deleniti dolorem temporibus iusto laborum et quasi tenetur sed.</p>
+        </div>
+        <div class="col-sm-6">
+            <h1>Bangka</h1>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Labore ab ea ipsam quis non deserunt sed ipsa! Libero, et. Quas dignissimos praesentium veniam vero voluptates temporibus iure fuga enim neque?</p>
+        </div>
+    </div>
+</body>
+</html>
--- bluehack2020/resources/views/index.blade.php
+++ bluehack2020/resources/views/index.blade.php
@@ -7,28 +7,130 @@
     <title>home</title>
     <link rel="stylesheet" href="{{ asset('/mdb/css/bootstrap.css')}}">
     <link rel="stylesheet" href="{{ asset('/mdb/css/bootstrap.min.css')}}">
-    <link rel="stylesheet" href="{{ asset('/mdb/css/bootstrap.js')}}">
-    <link rel="stylesheet" href="{{ asset('/mdb/css/bootstrap.min.js')}}">
+    <script src="{{ asset('/mdb/js/jquery-3.4.1.min.js')}}"></script>
+    <script src="{{asset('/mdb/js/bootstrap.js')}}"></script>
+    <script src="{{asset('/mdb/js/bootstrap.min.js')}}"></script>
     <link rel="stylesheet" href="{{ asset('/css/style.css')}}">
 </head>
 <body>
-    {{-- <nav style="background:blue;" class="navbar  navbar-dark primary-color">
-       <a class="navbar-brand" href="#">Title</a>
-       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span></button> 
-        <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav">
-                <li>
-                    <a class="nav-link" href="/dictionary">Dictionary</a>
-                </li>
-                <li>
-                    <a class="nav-link" href="/transportation">Reserve</a>
-                </li>
-            </ul>
+    <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img class="d-block w-100" src="{{asset('/img/subic-bay-philippines-volcano.jpg')}}"
+              alt="First slide">
+          </div>
+          <div class="carousel-item">
+            <img class="d-block w-100" src="{{asset('/img/shutterstock_634025597.jpg')}}"
+              alt="Second slide">
+          </div>
+          <div class="carousel-item">
+            <img class="d-block w-100" src="{{asset('/img/Phillippines_text.jpg')}}"
+              alt="Third slide">
+          </div>
         </div>
-    </nav> --}}
-    <div>
-        <img src="" alt="">
-    </div>
+        <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="sr-only">Next</span>
+        </a>
+      </div>
+      <div class="container">
+          <div>
+              <h1 style="text-align:center;">About</h1>
+              <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Id veritatis praesentium laboriosam rerum, perferendis itaque totam explicabo exercitationem nam delectus mollitia labore saepe cumque deleniti libero ducimus architecto amet? Vel! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nisi totam corporis est error eos odit itaque, consequuntur quibusdam repellendus nulla id facere quis dolorum quos consectetur veritatis. Corporis, dolor suscipit.</p>
+          </div>
+          <div>
+              <h1>Language</h1>
+              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus laborum voluptatibus dolorem voluptatem alias quo possimus impedit ut! Eaque tempore nulla, dolores corporis esse voluptatem optio commodi iste aspernatur dolore. Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio natus eius, nam dolorum amet impedit possimus, explicabo quod provident quam ratione harum velit dolores dolorem ipsa quibusdam voluptatibus animi aperiam!</p>
+              <button onclick="window.location.href='/dictionary'">Dictionary</button>
+          </div>
+          <div>
+              <h1>Transportation</h1>
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Enim libero, facilis voluptas alias molestiae quasi totam voluptate illo eius perferendis maxime ut nostrum amet similique soluta. Officia maiores mollitia quidem? Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum deserunt facere quisquam impedit quis minus accusamus repellat! Accusantium unde delectus, soluta culpa nobis odio quas, ullam qui, quisquam assumenda molestias?</p>
+          </div>
+          <button onclick="window.location.href='/trans'">Transportation</button>
+      </div>
+      <div class="container">
+        <div class="row blog">
+            <div class="col-md-12">
+                <div id="blogCarousel" class="carousel slide" data-ride="carousel">
+
+                    <ol class="carousel-indicators">
+                        <li data-target="#blogCarousel" data-slide-to="0" class="active"></li>
+                        <li data-target="#blogCarousel" data-slide-to="1"></li>
+                    </ol>
+
+                    <!-- Carousel items -->
+                    <div class="carousel-inner">
+
+                        <div class="carousel-item active">
+                            <div class="row">
+                                <div class="col-md-3">
+                                    <a href="#">
+                                        <img src="{{asset('/img/kalabaw1.jpg')}}" alt="Image" style="width:200px;height:200px;">
+                                    </a>
+                                </div>
+                                <div class="col-md-3">
+                                    <a href="#">
+                                        <img src="{{asset('/img/kalesa1.jpg')}}" alt="Image" style="width:200px;height:200px;">
+                                    </a>
+                                </div>
+                                <div class="col-md-3">
+                                    <a href="#">
+                                        <img src="{{asset('/img/etrik2.jpg')}}" alt="Image" style="width:200px;height:200px;">
+                                    </a>
+                                </div>
+                                <div class="col-md-3">
+                                    <a href="#">
+                                        <img src="{{asset('/img/habal1.jpg')}}" alt="Image" style="width:200px;height:200px;">
+                                    </a>
+                                </div>
+                            </div>
+                            <!--.row-->
+                        </div>
+                        <!--.item-->
+
+                        <div class="carousel-item">
+                            <div class="row">
+                                <div class="col-md-3">
+                                    <a href="#">
+                                        <img src="{{asset('/img/banka1.jpg')}}" alt="Image" style="width:200px;height:200px;">
+                                    </a>
+                                </div>
+                                <div class="col-md-3">
+                                    <a href="#">
+                                        <img src="{{asset('/img/kalabaw5.jpg')}}" alt="Image" style="width:200px;height:200px;">
+                                    </a>
+                                </div>
+                                <div class="col-md-3">
+                                    <a href="#">
+                                        <img src="{{asset('/img/kalesa2.jpg')}}" alt="Image" style="width:200px;height:200px;">
+                                    </a>
+                                </div>
+                                <div class="col-md-3">
+                                    <a href="#">
+                                        <img src="{{asset('/img/kalabaw2.jpg')}}" alt="Image" style="width:200px;height:200px;">
+                                    </a>
+                                </div>
+                            </div>
+                            <!--.row-->
+                        </div>
+                        <!--.item-->
+
+                    </div>
+                    <!--.carousel-inner-->
+                </div>
+                <!--.Carousel-->
+
+            </div>
+        </div>
+</div>
+<script>$('#blogCarousel').carousel({
+    interval: 5000
+});</script>
 </body>
+
 </html>
